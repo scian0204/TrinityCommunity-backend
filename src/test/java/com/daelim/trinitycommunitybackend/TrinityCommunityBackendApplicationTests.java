5cd0a9f32bd187b6753d2f61f2d84d22dc72a600
scian0204 <scian0204@gmail.com>
2023-09-16 20:01:03 +0900

    Init Project

---
+++ src/test/java/com/daelim/trinitycommunitybackend/TrinityCommunityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daelim.trinitycommunitybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrinityCommunityBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
